78f83ea3a5879b2315570d0c4911038f0ab2dcac
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 10:45:33 -0300

    feat: implement UpdateDeveloperRepository with sequelize

--- src/Infrastructure/db/sequelize/developer/DeveloperSequelizeRepository.ts
+++ src/Infrastructure/db/sequelize/developer/DeveloperSequelizeRepository.ts
@@ -1,10 +1,27 @@
 import { DeveloperModel } from '@/Domain/developer/Developer'
 import { CreateDeveloperRepository } from '@/Domain/developer/repositories/CreateDeveloperRepository'
+import { UpdateDeveloperRepository } from '@/Domain/developer/repositories/UpdateDeveloperRepository'
+import { UpdateDeveloperData } from '@/Domain/developer/usecases/UpdateDeveloper'
 import Developer from './Developer.sequelize'
 
-export class DeveloperSequelizeRepository implements CreateDeveloperRepository {
+export class DeveloperSequelizeRepository implements CreateDeveloperRepository, UpdateDeveloperRepository {
   async create (data: Omit<DeveloperModel, 'id'>): Promise<DeveloperModel> {
     const developer = await Developer.create(data)
     return developer
+  }
+
+  async update (id: string|number, data: UpdateDeveloperData): Promise<DeveloperModel> {
+    await Developer.update(data, {
+      where: {
+        id
+      }
+    })
+
+    const updatedDeveloper = await Developer.findOne({
+      where: {
+        id
+      }
+    })
+    return updatedDeveloper
   }
 }
