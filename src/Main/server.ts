fe7eb71f2e8d7242100492c80a889f866f2cdba3
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 09:24:41 -0300

    feat: add database sync before server initialization

--- src/Main/server.ts
+++ src/Main/server.ts
@@ -1,3 +1,11 @@
+import sequelizeConnectionInstance from '@/Infrastructure/db/sequelize/connection-instance'
 import app from './config/app'
+import 'reflect-metadata'
 
-app.listen(process.env.PORT, () => console.log('Server running on port: ' + process.env.PORT))
+sequelizeConnectionInstance.sync()
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log('Server running on port: ' + process.env.PORT))
+  })
+  .catch(() => {
+    console.log('Database sync error :(')
+  })
