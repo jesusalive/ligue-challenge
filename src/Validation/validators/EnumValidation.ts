4b56b1388bbe981ae33b827ff490009846297002
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 19:41:48 -0300

    feat: add nullable option to EnumValidation

--- src/Validation/validators/EnumValidation.ts
+++ src/Validation/validators/EnumValidation.ts
@@ -1,13 +1,19 @@
 import { Validation } from '@/Application/protocols'
 import { InvalidParamError } from '@/Domain/shared/errors'
+import { NullableOptions } from '../protocols/NullableOptions'
+
+interface EnumValidationOptions extends NullableOptions {}
 
 export class EnumValidation implements Validation {
   constructor (
     private readonly fieldName: string,
-    private readonly enumOptions: any[]
+    private readonly enumOptions: any[],
+    private readonly options?: EnumValidationOptions
   ) {}
 
   validate (input: any): Error {
+    if (!input[this.fieldName] && this.options?.nullable) return null
+
     for (const option of this.enumOptions) {
       if (option === input[this.fieldName]) return null
     }
