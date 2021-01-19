c10dd676cc1d9191a83bfa91d98a1bb415ef902e
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 09:38:56 -0300

    feat: add DateStringValidatorAdapter

---
+++ src/Infrastructure/validators/DateStringValidatorAdapter.ts
@@ -0,0 +1,10 @@
+import { DateValidator } from '@/Validation/protocols/DateValidator'
+
+import validator from 'validator'
+
+export class DateStringValidatorAdapter implements DateValidator {
+  isValid (dateString: string): boolean {
+    const date = validator.toDate(dateString)
+    return !!date
+  }
+}
