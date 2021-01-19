0661df0644231048919608ac6b2eaaa4858ee8d2
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 09:53:16 -0300

    fix: remove sequelize logs

--- src/Infrastructure/db/sequelize/connection-instance.ts
+++ src/Infrastructure/db/sequelize/connection-instance.ts
@@ -9,6 +9,7 @@ Sequelize.useCLS(cls.createNamespace('default-connection'))
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  logging: false,
   query: {
     raw: true
   }
