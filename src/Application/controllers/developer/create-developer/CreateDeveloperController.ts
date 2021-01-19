49fd9ae5dbcc217dfc1fdfcb05b95bffec5ba97b
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-18 21:20:45 -0300

    feat: ensure return badRequest if validation returns any error

--- src/Application/controllers/developer/create-developer/CreateDeveloperController.ts
+++ src/Application/controllers/developer/create-developer/CreateDeveloperController.ts
@@ -1,6 +1,5 @@
-import { serverError } from '@/Application/helpers/http/http-helper'
+import { badRequest, serverError } from '@/Application/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from '@/Application/protocols'
-
 export class CreateDeveloperController implements Controller {
   constructor (
     private readonly validation: Validation
@@ -8,7 +7,9 @@ export class CreateDeveloperController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body)
+      const err = this.validation.validate(httpRequest.body)
+      if (err) return badRequest(err)
+
       return null
     } catch (err) {
       return serverError(err)
--- src/Application/helpers/http/http-helper.ts
+++ src/Application/helpers/http/http-helper.ts
@@ -5,3 +5,7 @@ export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
 })
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
