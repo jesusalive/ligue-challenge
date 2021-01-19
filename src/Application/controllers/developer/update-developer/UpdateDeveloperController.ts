6c37be4bd71c4e833dca3baf8005dc57189f69ca
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 10:01:07 -0300

    feat: ensure return badRequest if validation returns any error

---
+++ src/Application/controllers/developer/update-developer/UpdateDeveloperController.ts
@@ -0,0 +1,16 @@
+import { badRequest } from '@/Application/helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse, Validation } from '@/Application/protocols'
+
+export class UpdateDeveloperController implements Controller {
+  constructor (
+    private readonly validation: Validation
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const err = this.validation.validate(httpRequest.body)
+
+    if (err) return badRequest(err)
+
+    return null
+  }
+}
