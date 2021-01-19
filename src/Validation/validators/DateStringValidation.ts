dabae5f183bd2683558a5436d0ed3f58be5813bc
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 19:35:51 -0300

    feat: add nullable option

--- src/Validation/validators/DateStringValidation.ts
+++ src/Validation/validators/DateStringValidation.ts
@@ -1,14 +1,20 @@
 import { Validation } from '@/Application/protocols'
 import { InvalidParamError } from '@/Domain/shared/errors'
 import { DateValidator } from '../protocols/DateValidator'
+import { NullableOptions } from '../protocols/NullableOptions'
+
+interface DateStringValidationOptions extends NullableOptions {}
 
 export class DateStringValidation implements Validation {
   constructor (
     private readonly fieldName: string,
-    private readonly dateValidator: DateValidator
+    private readonly dateValidator: DateValidator,
+    private readonly options?: DateStringValidationOptions
   ) {}
 
   validate (input: any): Error {
+    if (!input[this.fieldName] && this.options?.nullable) return null
+
     const isValid = this.dateValidator.isValid(input[this.fieldName])
 
     if (!isValid) {
