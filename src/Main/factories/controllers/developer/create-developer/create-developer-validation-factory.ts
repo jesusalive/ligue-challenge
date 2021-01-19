8bd97b20017518ac7f82785aca791895a65bfe11
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 09:49:24 -0300

    feat: add number and date validation on create-developer

--- src/Main/factories/controllers/developer/create-developer/create-developer-validation-factory.ts
+++ src/Main/factories/controllers/developer/create-developer/create-developer-validation-factory.ts
@@ -1,5 +1,8 @@
 import { Validation } from '@/Application/protocols'
+import { DateStringValidatorAdapter } from '@/Infrastructure/validators/DateStringValidatorAdapter'
+import { DateStringValidation } from '@/Validation/validators/DateStringValidation'
 import { EnumValidation } from '@/Validation/validators/EnumValidation'
+import { NumberValidation } from '@/Validation/validators/NumberValidation'
 import { RequiredFieldValidation } from '@/Validation/validators/RequiredFieldValidation'
 import { ValidationComposite } from '@/Validation/validators/ValidationComposite'
 
@@ -11,6 +14,8 @@ export const makeCreateDeveloperValidation = (): ValidationComposite => {
   validations.push(new RequiredFieldValidation('hobby'))
   validations.push(new RequiredFieldValidation('birthdate'))
   validations.push(new EnumValidation('sex', ['H', 'M']))
+  validations.push(new DateStringValidation('birthdate', new DateStringValidatorAdapter()))
+  validations.push(new NumberValidation('age'))
 
   return new ValidationComposite(validations)
 }
