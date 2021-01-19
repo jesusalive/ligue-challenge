30a0eec518aaa8f885e1797290db54a4c1f9caae
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 19:20:44 -0300

    feat: ensure throw a NotFoundError if UpdateDeveloperRepository returns null

--- src/Data/developer/update-developer/DbUpdateDeveloper.ts
+++ src/Data/developer/update-developer/DbUpdateDeveloper.ts
@@ -1,6 +1,7 @@
 import { DeveloperModel } from '@/Domain/developer/Developer'
 import { UpdateDeveloperRepository } from '@/Domain/developer/repositories/UpdateDeveloperRepository'
 import { UpdateDeveloper, UpdateDeveloperData } from '@/Domain/developer/usecases/UpdateDeveloper'
+import { NotFoundError } from '@/Domain/shared/errors/NotFoundError'
 
 export class DbUpdateDeveloper implements UpdateDeveloper {
   constructor (
@@ -9,6 +10,9 @@ export class DbUpdateDeveloper implements UpdateDeveloper {
 
   async update (id: string|number, data: UpdateDeveloperData): Promise<DeveloperModel> {
     const updatedDeveloper = await this.updateDeveloperRepository.update(id, data)
+
+    if (updatedDeveloper === null) throw new NotFoundError('Developer not found')
+
     return updatedDeveloper
   }
 }
