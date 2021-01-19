26697f72e40c4647cbe0b7701e5f814806cecc69
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 19:52:12 -0300

    feat: add nullable option to NumberValidation

--- src/Validation/validators/NumberValidation.ts
+++ src/Validation/validators/NumberValidation.ts
@@ -1,12 +1,18 @@
 import { Validation } from '@/Application/protocols'
 import { InvalidParamError } from '@/Domain/shared/errors'
+import { NullableOptions } from '../protocols/NullableOptions'
+
+interface NumberValidationOptions extends NullableOptions {}
 
 export class NumberValidation implements Validation {
   constructor (
-    private readonly fieldName: string
+    private readonly fieldName: string,
+    private readonly options?: NumberValidationOptions
   ) {}
 
   validate (input: any): Error {
+    if (!input[this.fieldName] && this.options?.nullable) return null
+
     if (isNaN(input[this.fieldName])) return new InvalidParamError(this.fieldName)
     return null
   }
