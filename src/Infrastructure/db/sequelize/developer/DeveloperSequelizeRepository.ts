7d8519ce8dc4e34b064a9406b1c0bc5dd72d214b
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 21:35:46 -0300

    feat: implement DeleteDeveloperByIdRepository with sequelize

--- src/Infrastructure/db/sequelize/developer/DeveloperSequelizeRepository.ts
+++ src/Infrastructure/db/sequelize/developer/DeveloperSequelizeRepository.ts
@@ -1,10 +1,14 @@
 import { DeveloperModel } from '@/Domain/developer/Developer'
 import { CreateDeveloperRepository } from '@/Domain/developer/repositories/CreateDeveloperRepository'
+import { DeleteDeveloperByIdRepository } from '@/Domain/developer/repositories/DeleteDeveloperByIdRepository'
 import { UpdateDeveloperRepository } from '@/Domain/developer/repositories/UpdateDeveloperRepository'
 import { UpdateDeveloperData } from '@/Domain/developer/usecases/UpdateDeveloper'
 import Developer from './Developer.sequelize'
 
-export class DeveloperSequelizeRepository implements CreateDeveloperRepository, UpdateDeveloperRepository {
+export class DeveloperSequelizeRepository implements
+  CreateDeveloperRepository,
+  UpdateDeveloperRepository,
+  DeleteDeveloperByIdRepository {
   async create (data: Omit<DeveloperModel, 'id'>): Promise<DeveloperModel> {
     const developer = await Developer.create(data)
     return developer
@@ -23,5 +27,15 @@ export class DeveloperSequelizeRepository implements CreateDeveloperRepository, 
       }
     })
     return updatedDeveloper
+  }
+
+  async deleteById (id: number): Promise<number> {
+    const quantityOfRemovedRecords = await Developer.destroy({
+      where: {
+        id
+      }
+    })
+
+    return quantityOfRemovedRecords
   }
 }
