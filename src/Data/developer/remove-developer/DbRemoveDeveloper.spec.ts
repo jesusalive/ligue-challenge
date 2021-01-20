c4db9c99149c19237d53402c1ebd8451444e952c
Matheus Placido Ferreira Gomes <matheusejesuss2@outlook.com>
2021-01-19 21:14:05 -0300

    test: ensure throw if DeleteDeveloperByIdRepository throws

--- src/Data/developer/remove-developer/DbRemoveDeveloper.spec.ts
+++ src/Data/developer/remove-developer/DbRemoveDeveloper.spec.ts
@@ -1,4 +1,5 @@
 import { DeleteDeveloperByIdRepository } from '@/Domain/developer/repositories/DeleteDeveloperByIdRepository'
+import { NotFoundError } from '@/Domain/shared/errors/NotFoundError'
 import { DbRemoveDeveloper } from './DbRemoveDeveloper'
 
 const makeDeleteDeveloperByIdRepositoryStub = (): DeleteDeveloperByIdRepository => {
@@ -33,5 +34,16 @@ describe('DbAddDeveloper', () => {
     await sut.remove('any_id')
 
     expect(deleteSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should throw if DeleteDeveloperByIdRepository throws', async () => {
+    const { sut, deleteDeveloperByIdRepositoryStub } = makeSut()
+
+    const deleteSpy = jest.spyOn(deleteDeveloperByIdRepositoryStub, 'deleteById')
+    deleteSpy.mockRejectedValueOnce(new NotFoundError('any_message'))
+
+    const promise = sut.remove('any_id')
+
+    await expect(promise).rejects.toEqual(new NotFoundError('any_message'))
   })
 })
